f5d19c99e5946db3ed6f85a2593e198f0ea62f62
　”liyang“ <liyang.ok@outlook.com>
2022-07-17 23:16:30 +0800

    init

---
+++ app/src/main/java/com/top/compose/sample/rn/LoadReactActivity.kt
@@ -0,0 +1,11 @@
+package com.top.compose.sample.rn
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoadReactActivity: AppCompatActivity() {
+
+   // private val mReactNativeHost: ReactNativeHost? = null
+
+
+}
